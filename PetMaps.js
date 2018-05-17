45df45a1f0092023c960d2984519bcef29835053
Francia Moran <franciam115@gmail.com>
2018-05-17 13:47:49 -0500

    Final changes

--- PetMaps.js
+++ PetMaps.js
@@ -9,153 +9,178 @@ function getStartedButton() {
       event.preventDefault();
       $('#next-page').html(searchPage());
       generateLocation();
-  });
+      });
 }
  
 $(getStartedButton)
 
-///////////////////////////////////////////////////////////////// 
-
 function searchPage () {
   return `
-  <p id="description-paragraph2">Enter search criteria and the location you would like to search below:</p>
-  <form action="#" id="js-search-form">
-    <select id="animal-type">
-  <option value="dog">Dog</option>
-  <option value="cat">Cat</option>
-</select>
-  <select id="age">
-  <option value="Baby">Baby</option>
-  <option value="Young">Young</option>
-  <option value="Adult">Adult</option>
-  <option value="Senior">Senior</option>
-</select>
- <select id="B-or-S">
-  <option value="S">Small</option>
-  <option value="M">Medium</option>
-  <option value="L">Large</option>
-  <option value="XL">Extra-Large</option>
-</select>
-<select id="M-or-F">
-  <option value="M">Male</option>
-  <option value="F">Female</option>
-</select>
-      <br>
-      <label for="query">Search Location  Here:</label>
-            <input type="text" id="js-query" class="controls" name="search" aria-label="search-here" placeholder="enter location here">
-            <button id="search-button" type="submit">Find Animals</button>
+</iframe aria-live="assertive" title="Area to search animals">
+  <html lang="en">
+    <main role="main">
+      <p id="description-paragraph2">Enter search criteria and the location you would like to search below:</p>
+        <form action="#" id="js-search-form">
+          <label class="labels" for="animal-type">Animal:</label>
+            <select id="animal-type" aria-live="assertive">
+              <option value="dog">Dog</option>
+              <option value="cat">Cat</option>
+            </select>
+          <label class="labels" for="age">Age:</label>
+            <select aria-live="assertive" id="age">
+              <option value="Baby">Baby</option>
+              <option value="Young">Young</option>
+              <option value="Adult">Adult</option>
+              <option value="Senior">Senior</option>
+            </select>
+          <label class="labels" for="B-or-S">Size:</label>
+            <select aria-live="assertive" id="B-or-S">
+              <option value="S">Small</option>
+              <option value="M">Medium</option>
+              <option value="L">Large</option>
+              <option value="XL">Extra-Large</option>
+            </select>
+          <label class="labels" for="M-or-F">Sex:</label>
+            <select aria-live="assertive" id="M-or-F">
+              <option value="M">Male</option>
+              <option value="F">Female</option>
+            </select>
             <br>
-      <span id="error"></span>
-    </form>
-      <p id="results-number">Results:</p>
-      <div aria-live="assertive" id="animal-data"></div>
-    <div aria-live="assertive" id="map"></div>
+          <label class="labels" for="js-query">Search City Here:</label>
+            <input type="text" id="js-query" class="controls" name="search" aria-label="search-here" placeholder="Enter city here">
+              <button id="search-button" type="submit">Find Animals</button>
+              <br>
+              <span id="error"></span>
+        </form>
+        <div aria-live="assertive" id="map"></div>
+        <div aria-live="assertive" id="animal-data"></div>
+    </main>
+  </html>
+</iframe>
   `;
-}
-
-///////////////////////////////////////////////////////////////// 
-
+} 
 
 function initAutocomplete() {
-      $('#next-page').submit('#search-button', function(event){
-        event.preventDefault();
-        const queryTarget = $(event.currentTarget).find('#js-query');
+  $('#next-page').submit('#search-button', function(event){
+    event.preventDefault();
+    const queryTarget = $(event.currentTarget).find('#js-query');
     const queryT = queryTarget.val();
-   
-         queryTarget.val("");
-         console.log(queryT);
-          
+    queryTarget.val("");
     if (queryT){
       getPetFinderAPI(queryT);
-      $('#error').text("");
+      $('#error').text("")
     }
     else {
-      $('#error').text("Please enter a location.");
+      $('#error').text("Please enter a location")
     }
   });
 }
-
-/////////////////////////////////////////////////////////////////
+ 
 function generateLocation () {
-    map = new google.maps.Map(document.getElementById('map'), {
+    map = new google.maps.Map(document.getElementById('map'),
+        {
           zoom: 11,
           center: {lat: -34.397, lng: 150.644}
         });
-        geocoder = new google.maps.Geocoder();
+         geocoder = new google.maps.Geocoder();
         var input = document.getElementById('js-query');
         var autocomplete = new google.maps.places.Autocomplete(input);
-
         document.getElementById('search-button').addEventListener('click', function() {
-          
           geocodeAddress(geocoder, map);
-          
         });
-      }
+};
 
-
-
-
-  function geocodeAddress(geocoder, map) {
+function geocodeAddress(geocoder, map) {
     var address = document.getElementById('js-query').value;
-        geocoder.geocode({'address': address}, function(results, status) {
+    if (address) {
+       geocoder.geocode({'address': address}, function(results, status) {
           if (status === 'OK') {
             map.setCenter(results[0].geometry.location);
-            var marker = new google.maps.Marker({
-              map: map,
-              position: results[0].geometry.location
-              //`${eachPet.contact.address1.$t}`
-              //('our-test-address')
-              
-            });
-          } else {
+          } 
+          else {
             alert('Geocode was not successful for the following reason: ' + status);
           }
         });
-  }
+    }
+}
       
-/////////////////////////////////////////////////////////////////    
-      
-      
-      
-
-        
 function displayAnimalData (eachPet) {
-  
-  var eachPetAddress = eachPet.contact.address1.$t + eachPet.contact.city.$t + eachPet.contact.state.$t + eachPet.contact.zip.$t;
-  
-  geocoder.geocode({'address': eachPetAddress}, function(results, status) {
-        if (status === 'OK') {
-          map.setCenter(results[0].geometry.location);
-          var marker = new google.maps.Marker({
+  let eachPetAddress = "";
+  if (eachPet.contact.address1.$t) {
+    eachPetAddress += eachPet.contact.address1.$t; 
+  }
+  if (eachPet.contact.city.$t) {
+    eachPetAddress += eachPet.contact.city.$t;
+  }
+  if (eachPet.contact.state.$t) {
+    eachPetAddress += eachPet.contact.state.$t;
+  }
+  if (eachPet.contact.zip.$t) {
+    eachPetAddress += eachPet.contact.zip.$t;
+  }
+  geocoder.geocode({'address': eachPetAddress}, function(results, status) 
+    {
+    if (status === 'OK') {
+      map.setCenter(results[0].geometry.location);
+      var marker = new google.maps.Marker({
               map: map,
               position: results[0].geometry.location
-              //`${eachPet.contact.address1.$t}`
-              //('our-test-address')
-              
           });
-        } else {
+    } 
+    else {
             alert('Geocode was not successful for the following reason: ' + status);
-           
-        }
-  });
+    }
+    let infoWindowString = `<div id="popup-box">
+    <h1 id="first-heading">${eachPet.name.$t.replace(/"/g, ' ')}</h1>`;
+    if (eachPet.media.photos) {
+      infoWindowString += `<img id="animal-pics" src="${eachPet.media.photos.photo[2].$t}" alt="picture of the adoptable animal"/>`;
+    }
+    infoWindowString += `<p>${eachPetAddress}</p></div>`;
+    let infoWindow = new google.maps.InfoWindow({
+          content: infoWindowString  
+      }); 
+    google.maps.event.addListener(marker, 'click', function() {
+          infoWindow.open(map, marker)
+       });      
+    $('#next-page').on("click", '#' + eachPet.name.$t.replace(/`/g, ' '), function() {
+          infoWindow.open(map, marker);
+        }); 
+    });
 
-  return `
+ try {
+      let initialString1 = `
+     <div id="animal-results">
+      <hr>
+      <h3 id=${eachPet.name.$t.replace(/`/g, ' ')}>${eachPet.name.$t.replace(/`/g, ' ')}</h3>`;
+      if (eachPet.media.photos.photo[2].$t) {
+        initialString1 += `<img id="animal-pics" src="${eachPet.media.photos.photo[2].$t}" alt="picture of the adoptable animal"/>`;
+      }
+      if (eachPet.description.$t) {
+      initialString1 += `<p>${eachPet.description.$t}</p>`;
+      }
+      if (eachPetAddress) {
+      initialString1 += `<p>${eachPetAddress}</p>`;
+      }
+      initialString1 += `</div>`;
+      return initialString1;
+  } 
+  
+  catch (err)  {
+      let initialString2 = `
       <div id="animal-results">
       <hr>
-      <p>${eachPet.name.$t}</p>
-      <img src="${eachPet.media.photos.photo[2].$t}"/>
-      <p>${eachPet.description.$t}</p>
-      <p id="our-test-address">${eachPet.contact.address1.$t}</p>
-      <p>${eachPetAddress}</p>
-          </div>
-  `;
+      <h3 id=${eachPet.name.$t.replace(/`/g, ' ')}>${eachPet.name.$t.replace(/`/g, ' ')}</h3>`;
+      if (eachPet.description.$t) {
+        initialString2 += `<p>${eachPet.description.$t}</p>`;
+      }
+      if (eachPetAddress) {
+        initialString2 += `<p>${eachPetAddress}</p>`;
+      }
+      initialString2 += `</div>`;
+      return initialString2;
+      } 
 }      
-      
-          
-      
-
-
-  
+     
 function getPetFinderAPI (queryT) {
   let animalType = $('#animal-type').val();
   let theAge = $('#age').val();
@@ -163,7 +188,8 @@ function getPetFinderAPI (queryT) {
   let theSex = $('#M-or-F').val();
     const settings = {
       url: PETFINDER_URL,
-      data: {
+      data: 
+      {
       format: `json`,
       key: `194d0a2a4598ae25c7edd35d4640c37f`,
       location: queryT,
@@ -176,7 +202,6 @@ function getPetFinderAPI (queryT) {
       dataType: `jsonp`,
       type: `GET`,
       success: function (data){
-        console.log(data);
         var resultsHTML = "";
         for (var i=0; i < data.petfinder.pets.pet.length; i++){
           var eachPet = data.petfinder.pets.pet[i];
@@ -184,7 +209,6 @@ function getPetFinderAPI (queryT) {
           resultsHTML += eachPetHTML;
         }
         $('#animal-data').html(resultsHTML);
-      
     }
     };
   $.ajax(settings);
